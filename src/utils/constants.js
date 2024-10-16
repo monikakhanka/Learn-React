babdd43de50ef4677221ad5e317217cd7f66bbfd
monika <monikakhanka@gmail.com>
2024-10-16 22:45:59 +0530

    cors

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,4 +2,4 @@ export const CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_l
 
 export const LOGO_URL ="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQdaMYtVi9_tfNcpsbGGseU6ehYgV9UeU3h7A&s";
 
-export const MENU_API = "/api/menu/:restaurantId";
+export const MENU_API = "/api/menu/";
