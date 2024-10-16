7948660b23a2a3a9e51a5a768c1a8fd969c85ad8
monika <monikakhanka@gmail.com>
2024-10-16 22:40:06 +0530

    cors

--- src/utils/useRestaurantMenu.js
+++ src/utils/useRestaurantMenu.js
@@ -1,7 +1,8 @@
 import {useState, useEffect} from "react";
 import { MENU_API } from "./constants";
 const useRestaurantMenu = (resId) => {
-
+    const full_url = `${MENU_API}${resId}`;
+    console.log(full_url);
     const [resInfo, setResInfo] = useState(null);
     
     useEffect(()=>{
